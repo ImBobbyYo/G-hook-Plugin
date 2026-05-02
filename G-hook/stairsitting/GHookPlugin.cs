d0e5d97ec91b0a5deac55bd009b411e17ff05da4
swerto.png <bobbyshoenfelt2@gmail.com>
2026-05-02 00:48:13 -0400

    Update GHookPlugin.cs

--- G-hook/stairsitting/GHookPlugin.cs
+++ G-hook/stairsitting/GHookPlugin.cs
@@ -2,19 +2,22 @@ using Minecraft.Server.FourKit;
 using Minecraft.Server.FourKit.Plugin;
 using Minecraft.Server.FourKit.Command;
 
-public class GHookPlugin : ServerPlugin
+namespace GrapplingHook
 {
-    public override string name => "G-Hook";
-    public override string version => "1.0.0";
-    public override string author => "Bobby";
-
-    public override void onEnable()
+    public class GHookPlugin : ServerPlugin
     {
-        FourKit.addListener(new GHookListener());
-        FourKit.getCommand("ghook").setExecutor(new GHookCommand());
-        FourKit.getCommand("ghook").setDescription("Gives a grappling hook item.");
-        FourKit.getCommand("ghook").setUsage("/ghook give [player]");
-    }
+        public override string name => "G-Hook";
+        public override string version => "1.0.1";
+        public override string author => "Bobby";
 
-    public override void onDisable() { }
+        public override void onEnable()
+        {
+            FourKit.addListener(new GHookListener());
+            FourKit.getCommand("ghook").setExecutor(new GHookCommand());
+            FourKit.getCommand("ghook").setDescription("Gives a grappling hook item.");
+            FourKit.getCommand("ghook").setUsage("/ghook give [player]");
+        }
+
+        public override void onDisable() { }
+    }
 }
